d4fb2116928ee261e5d9180fe5e19c22f435b8c0
quangk22 <113655587+quangk22@users.noreply.github.com>
2022-10-06 08:26:47 +0700

    Add files via upload

---
+++ Variable/src/basic/dev/MainApp.java
@@ -0,0 +1,23 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String firstName = "phan van  ";
+      String lastName = "nam";
+      int old = 15;
+      float markMath = 9f;
+      boolean passed = true ;
+      
+      System.out.println("Ho va ten :");
+      System.out.println(firstName + " " + lastName);
+      System.out.println("Mark Math :" + markMath);
+      System.out.println("Passed : "+ passed);
+      
+      
+      final float PI = 3.14f;
+      
+	}
+
+}
